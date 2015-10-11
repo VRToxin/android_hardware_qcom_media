5db59176b0c8145b32952b8aa157c1aa13ddd216
Christopher N. Hesse <raymanfx@gmail.com>
2015-10-11 21:32:58 +0200

    mm-video-v4l2: Ifdef session priority and operatingrate features
    Change-Id: Ie7cbc84500752acd2d67dbb1c5199214d22f0ee2

--- mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
+++ mm-video-v4l2/vidc/venc/src/video_encoder_device_v4l2.cpp
@@ -4708,6 +4708,7 @@ bool venc_dev::venc_set_vpx_error_resilience(OMX_BOOL enable)
 bool venc_dev::venc_set_session_priority(OMX_U32 priority) {
     struct v4l2_control control;
 
+#ifdef V4L2_CID_MPEG_VIDC_VIDEO_PRIORITY
     control.id = V4L2_CID_MPEG_VIDC_VIDEO_PRIORITY;
     switch(priority) {
         case 0:
@@ -4734,11 +4735,15 @@ bool venc_dev::venc_set_session_priority(OMX_U32 priority) {
     DEBUG_PRINT_LOW("Success IOCTL set control for id=%x, val=%d",
             control.id, control.value);
     return true;
+#else
+    return false;
+#endif
 }
 
 bool venc_dev::venc_set_operatingrate(OMX_U32 rate) {
     struct v4l2_control control;
 
+#ifdef V4L2_CID_MPEG_VIDC_VIDEO_OPERATING_RATE
     control.id = V4L2_CID_MPEG_VIDC_VIDEO_OPERATING_RATE;
     control.value = rate;
 
@@ -4754,6 +4759,9 @@ bool venc_dev::venc_set_operatingrate(OMX_U32 rate) {
     operating_rate = rate;
     DEBUG_PRINT_LOW("Operating Rate Set = %d fps",  rate >> 16);
     return true;
+#else
+    return false;
+#endif
 }
 
 bool venc_dev::venc_get_profile_level(OMX_U32 *eProfile,OMX_U32 *eLevel)
